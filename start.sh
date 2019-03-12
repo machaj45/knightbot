61c33f0b447f63903f6179f7d9801858174ecf4a
machaj45 <machaj45@fel.cvut.cz>
2019-03-12 20:24:03 +0100

    Final touch befor passing to another teamate. Updating start.sh and sesion

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MY_PATH=~/kinect_workspace/src/openni_tracker/tmux_scripts
+MY_PATH=~/workspace/src/ros_packages
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
 # remove the old link
@@ -11,3 +11,4 @@ ln $MY_PATH/session.yml $MY_PATH/.tmuxinator.yml
 # start tmuxinator
 cd $MY_PATH
 tmuxinator
+cd $HOME
