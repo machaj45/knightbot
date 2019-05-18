3b3a94ba8101f001b767c3f5c0d758a87ee00e8f
machaj45 <machaj45@fel.cvut.cz>
2019-05-18 21:23:17 +0200

    RGB sensor is now initialized and servos are initialized too.  Changes to be committed: 	modified:   Config.h 	modified:   CubeCollector.ino

--- CubeCollector/Config.h
+++ CubeCollector/Config.h
@@ -12,5 +12,8 @@
 
 
 // ---------------------- Communication -------------------------
-
+// usable for interrupts 2, 3, 18, 19, 20, 21
+#define DEBUG = true
+#define NUMBER_OF_SEVOS = 4
+#define OFFSET = 9
 #define SERIAL_BAUDRATE 115200
