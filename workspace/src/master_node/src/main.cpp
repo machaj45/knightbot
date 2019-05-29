c37fafb7094e043f46d8ea2fbb064f58b35945aa
Aleš Novotný <novotal8@fel.cvut.cz>
2019-05-29 18:52:49 +0200

    Service for commanding motors added

--- workspace/src/master_node/src/main.cpp
+++ workspace/src/master_node/src/main.cpp
@@ -1,28 +1,88 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_srvs/SetBool.h"
+#include "master_node/arduino_srv.h"
+
+
+ros::ServiceClient client;
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+  /*
+master_node::arduino_srv srv;
+srv.request.cmd = 4;
+srv.request.param = 20;
+
+
+if( client.call(srv)){
+ROS_INFO("OK");
+}else{
+ROS_INFO("ERROR");
 }
+
+ROS_INFO("Move robot, move!");
+*/
+}
+
+
 
 bool  btn1(std_srvs::SetBool::Request  &req, std_srvs::SetBool::Response &res)
 {
 res.success = true;
 res.message = "Button 1 was pressed";
 ROS_INFO("request: x=%d", (int)req.data);
+  
+/*
+master_node::arduino_srv srv;
+  srv.request.cmd = 4;
+  srv.request.param = 20;
+  client.call(srv);
+
+ROS_INFO("Move robot, move!");
+*/
   return true;
 }
 
+bool fake(master_node::arduino_srv::Request  &req, master_node::arduino_srv::Response &res)
+{
+
+res.success = true;
+res.message = "Robot was offended and go away";
+ROS_INFO("CMD=%d  VEL=%d ", req.cmd, req.param);
+  return true;
+}
+
+
 int main(int argc, char **argv)
 {
-ros::init(argc, argv, "listener");
+ros::init(argc, argv, "master_robot_node");
 ros::NodeHandle n;
+client = n.serviceClient<master_node::arduino_srv>("i_am_fake_robot",true);
+
 ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-ros::ServiceServer client = n.advertiseService("btn1", btn1);
-  
+ros::ServiceServer server = n.advertiseService("btn1", btn1);
+ros::ServiceServer server2 = n.advertiseService("i_am_fake_robot", fake);
+
 ros::spin();
+
+//master_node::arduino_srv srv;
+
+
+/*
+ros::Rate r(1); // 10 hz
+while (ros::ok())
+{
+master_node::arduino_srv srv;
+
+  srv.request.cmd = 4;
+  srv.request.param = 20;
+ROS_INFO("I am here");  
+client.call(srv);
+
+  r.sleep();
+}*/
 
   return 0;
 }
