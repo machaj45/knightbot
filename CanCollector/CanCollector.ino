bb86113d8710af09607e6d48a04e68bac4cbb52a
nemecm42 <nemecm42@fel.cvut.cz>
2019-05-30 20:49:27 +0200

    CanController now picks cans, Display update

--- CanCollector/CanCollector.ino
+++ CanCollector/CanCollector.ino
@@ -2,15 +2,22 @@
 #include "Config.h"
 #include <Servo.h>
 
+#define PICK 0
+#define DEPLOY 1
+#define MOVING 2
+
 Servo magServo;
 boolean releaseCanAction = false;
 long canTime = 0;
 
+boolean motorsInited = false;
 
 // mode of collector:
 //false: ready to collect cans
 //true: ready to deploy cans
 boolean mode = false;
+
+boolean operatingCan = false;
 
 byte cansCollected = 0;
 
@@ -19,7 +26,7 @@ boolean readyToPop = false;
 
 //motor endstops:
 //rot-gear start, rot-gear end, elevator top, elevator bot, sun encoder
-boolean endstops[] = {false, false, false, false};
+boolean endstops[] = {false, false, false, false,false};
 boolean canPresent = false;
 
 void setup() {
@@ -28,6 +35,9 @@ void setup() {
   pinMode(ROT_GEAR_START_PIN, INPUT_PULLUP);
   pinMode(ROT_GEAR_END_PIN, INPUT_PULLUP);
   pinMode(ELEVATOR_ENDSTOP_PIN, INPUT_PULLUP);
+  pinMode(ELEVATOR_END_PIN, INPUT_PULLUP);
+  pinMode(ELEVATOR_END_GND_PIN, OUTPUT);
+  digitalWrite(ELEVATOR_END_GND_PIN, LOW);
   pinMode(CAN_CHECK_PIN, INPUT);
   magServo.attach(MAG_SERVO_PIN);
   pinMode(SUN_MOT_PIN1, OUTPUT);
@@ -42,39 +52,41 @@ void setup() {
   //pushElevator();
   //rotateSun(false);
   //rotateRotGear(true);
-  //rotateSun(false);
+  //rotateSun(true);
   //releaseCan();
-  rotateElevator(false);
+  //rotateElevator(false);
+  //motorsInited = true;
 }
 
 void loop() {
+
+  if(!motorsInited){
+    initMotors();
+  }else{
+    updateStateMachine();
+  }
+  
   updateServo();
   //updateCom();
   updateSensors();
   updateMotors();
-//  Serial.print(endstops[0]);
-//  Serial.print("  ");
-//  Serial.print(endstops[1]);
-//  Serial.print("  ");
-//  Serial.print(endstops[2]);
-//  Serial.print("  ");
-//  Serial.println(endstops[3]);
 }
 
 void updateSensors(){
   endstops[0] = digitalRead(ROT_GEAR_START_PIN) == LOW;
   endstops[1] = digitalRead(ROT_GEAR_END_PIN) == LOW;
   endstops[2] = digitalRead(ELEVATOR_ENDSTOP_PIN) == LOW;
-  endstops[3] = analogRead(SUN_ENC_PIN) > SUN_ENC_TRES;
-  canPresent = digitalRead(CAN_CHECK_PIN) == HIGH;
+  endstops[3] = digitalRead(ELEVATOR_END_PIN) == LOW;
+  endstops[4] = analogRead(SUN_ENC_PIN) > SUN_ENC_TRES;
+  canPresent = digitalRead(CAN_CHECK_PIN) == LOW;
 }
 
 void getReadyToPush(){
-  
+  mode = false;// must be changed only when not operating can
 }
 
 void getReadyToPop(){
-  
+  mode = true;
 }
 
 void pushCan(){
@@ -103,4 +115,18 @@ void updateServo(){
       releaseCanAction = false;
     }
   }
+}
+
+void printEndstops(){
+  Serial.print(endstops[0]);
+  Serial.print("  ");
+  Serial.print(endstops[1]);
+  Serial.print("  ");
+  Serial.print(endstops[2]);
+  Serial.print("  ");
+  Serial.print(endstops[3]);
+  Serial.print("  ");
+  Serial.print(endstops[4]);
+  Serial.print("  ");
+  Serial.println(canPresent);
 }
--- CanCollector/Motors.ino
+++ CanCollector/Motors.ino
@@ -1,6 +1,10 @@
 
 #include "Config.h"
 
+#define ROTGEAR 0
+#define ELEV 1
+#define SUN 2
+
 // 0-rotGear, 1-elevator, 2-sun
 boolean isMoving[] = {false, false, false};
 boolean movDir[] = {false, false, false};
@@ -8,6 +12,172 @@ boolean movDir[] = {false, false, false};
 long sunMovStart = 0;
 long elevMovStart = 0;
 long pushElevStart = 0;
+long sleepTime = 0;
+
+int initState = 0;
+
+int state = 0;
+int substate = 0;
+
+void initMotors(){
+  switch(initState){
+    case 0:
+        rotateElevator(false);
+        initState++;
+      break;
+    case 1:
+        if(!isMoving[ELEV]){
+          initState++;
+        }
+      break;
+    case 2:
+        rotateRotGear(false);
+        rotateSun(false);
+        initState++;
+      break;
+    case 3:
+        if(!isMoving[ROTGEAR] && !isMoving[SUN]){
+          initState++;
+        }
+      break;
+    case 4:
+        rotateElevator(true);
+        initState++;
+      break;
+    case 5:
+        if(!isMoving[ELEV]){
+          initState++;
+        }
+      break;
+    case 6:
+        motorsInited = true;
+        state = 2;
+        substate = 6; //set to last !!!
+      break;
+  }
+}
+
+void updateStateMachine(){
+   switch(state){
+      case PICK:
+          switch(substate){
+              case 0:
+                  pushElevator();
+                  substate++;
+                break;
+              case 1:
+                  if(pushElevStart == 0){
+                    substate++;
+                  }
+                break;
+              case 2:
+                  rotateElevator(false);
+                  substate++;
+                break;
+              case 3:
+                  if(!isMoving[ELEV]){
+                    substate++;
+                  }
+                break;
+              case 4:
+                  rotateRotGear(true);
+                  rotateSun(true);
+                  substate++;
+                break;
+              case 5:
+                  if(!isMoving[ROTGEAR] && !isMoving[SUN]){
+                    substate++;
+                  }
+                break;
+              case 6:
+                  releaseCan();
+                  cansCollected++;
+                  substate++;
+                  sleepTime = millis();
+                break;
+              case 7:
+                  if(millis() - sleepTime >= 1000){
+                    state = MOVING;
+                    substate = 0;
+                  }
+                break;
+            }
+        break;
+      case DEPLOY:
+          switch(substate){
+              case 0:
+                  pushElevator();
+                  substate++;
+                break;
+              case 1:
+                  if(pushElevStart == 0){
+                    substate++;
+                  }
+                break;
+              case 2:
+                  rotateRotGear(false);
+                  rotateSun(false);
+                  substate++;
+                break;
+              case 3:
+                  if(!isMoving[ROTGEAR] && !isMoving[SUN]){
+                    substate++;
+                  }
+                break;
+              case 4:
+                  
+                break;
+              case 5:
+                  
+                break;
+            }
+        break;
+      case MOVING:
+          if(!mode){ //e.g. ready for push
+            switch(substate){
+              case 0:
+                  rotateElevator(false);
+                  substate++;
+                break;
+              case 1:
+                  if(!isMoving[ELEV]){
+                    substate++;
+                  }
+                break;
+              case 2:
+                  rotateRotGear(false);
+                  substate++;
+                break;
+              case 3:
+                  if(!isMoving[ROTGEAR]){
+                    substate++;
+                  }
+                break;
+              case 4:
+                  rotateElevator(true);
+                  substate++;
+                break;
+              case 5:
+                  if(!isMoving[ELEV]){
+                    substate++;
+                  }
+                break;
+              case 6:
+                  operatingCan = false;
+                  if(canPresent && cansCollected <= 8){
+                    state = PICK;
+                    substate = 0;
+                    operatingCan = true;
+                  }
+                break;
+            }
+          }else{
+            
+          }
+        break;
+    }
+}
+
 
 void updateMotors(){
   if(isMoving[0] && ((movDir[0] && endstops[0]) || (!movDir[0] && endstops[1]))){
@@ -15,12 +185,12 @@ void updateMotors(){
     analogWrite(ROT_GEAR_MOT_PIN2, 0);
     isMoving[0] = false;
   }
-  if(isMoving[1] && (millis()-elevMovStart > 1000) && endstops[2]){
+  if(isMoving[1] && ((movDir[1] && endstops[2]) || (!movDir[1] && endstops[3]))){
     digitalWrite(ELEVATOR_MOT_PIN1, LOW);
     digitalWrite(ELEVATOR_MOT_PIN2, LOW);
     isMoving[1] = false;
   }
-  if(isMoving[2] && (millis()-sunMovStart > 500) && endstops[3]){
+  if(isMoving[2] && (millis()-sunMovStart > 500) && endstops[4]){
     analogWrite(SUN_MOT_PIN1, 0);
     analogWrite(SUN_MOT_PIN2, 0);
     isMoving[2] = false;
@@ -45,14 +215,13 @@ void rotateRotGear(boolean dir){
 
 //dir - true = down
 void rotateElevator(boolean dir){
-  //if((dir && !endstops[2])||(!dir && !endstops[3])){
-  //TODO timing start run
+  if(!isMoving[1]){
     movDir[1] = dir;
     isMoving[1] = true;
     elevMovStart = millis();
     digitalWrite(ELEVATOR_MOT_PIN1, dir?HIGH:LOW);
     digitalWrite(ELEVATOR_MOT_PIN2, dir?LOW:HIGH);
-  //}
+  }
 }
 
 //dir - true = clockwise
